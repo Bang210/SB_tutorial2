0cd0f79034bdfb92ac8d239fbccac7d89585c494
Bang210 <bangvelope@gmail.com>
2023-11-16 16:29:41 +0900

    start

---
+++ src/main/java/com/ll/sb_tutorial2/SbTutorial2Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb_tutorial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTutorial2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbTutorial2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb_tutorial2/SbTutorial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb_tutorial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTutorial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
