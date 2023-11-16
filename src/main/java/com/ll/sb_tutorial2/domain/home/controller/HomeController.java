ed4e74dffbe7696ea44c220ec5519e37ec147590
Bang210 <bangvelope@gmail.com>
2023-11-16 22:06:09 +0900

    test1

--- src/main/java/com/ll/sb_tutorial2/domain/home/controller/HomeController.java
+++ src/main/java/com/ll/sb_tutorial2/domain/home/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb_tutorial2.domain.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,6 +18,13 @@ public class HomeController {
     @ResponseBody
     String showAbout() {
         return "연습용 프로젝트입니다.";
+    }
+
+    @GetMapping("/test1")
+    @ResponseBody
+    String showTest1(@RequestParam(defaultValue = "0") int a,
+                  @RequestParam(defaultValue = "0") int b) {
+        return "계산결과: %d".formatted(a + b);
     }
 
 }
